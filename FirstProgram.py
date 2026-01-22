cd2a1d10c77ef57202ffdcda2a21ecddbe8628a2
emmabb426 <ebarnes17@unomaha.edu>
2026-01-22 18:59:25 +0000

    Here is my First Program file.

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,19 +1,20 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Emma Barnes
+#Date:01/22/2026
+#Assignment: Lab 01
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  user_name = input("Please give your name: ")
   #Use the user's name in the program.
-
+  print(f"The user's name is {user_name}.")
   #Ask the user for their age.
-
+  age = int(input("Please give your age: "))
   #Tell the user what year they were born in.
+  print(f"You were born in {2025-age}")
   #Assume that they have not had their birthday yet this year.
 
 
